4b154887a4df70b27719204c4ae855f6ceadbb93
Alif Wibowo <alif.likes.coding@gmail.com>
2022-10-26 16:48:31 +0700

    Make javascript button variables and change id name

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Non Action Buttons */
+let one = document.querySelector('#one');
+let two = document.querySelector('#two');
+let three = document.querySelector('#three');
+let four = document.querySelector('#four');
+let five = document.querySelector('#five');
+let six = document.querySelector('#six');
+let seven = document.querySelector('#seven');
+let eight = document.querySelector('#eight');
+let nine = document.querySelector('#nine');
+let zero = document.querySelector('#zero');
+let dot = document.querySelector('#dot');
+
+/* Action Buttons */
+let clear = document.querySelector('#clear');
+let backSpace = document.querySelector('#backspace');
+let plus = document.querySelector('#plus');
+let minus = document.querySelector('#minus');
+let times = document.querySelector('#times');
+let divide = document.querySelector('#divide');
+let equal = document.querySelector('#equal');
+let mode = document.querySelector('#color-mode');
