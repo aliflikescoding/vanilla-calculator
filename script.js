899ece930dda20da0f90df23df595b25688f5dd8
Alif Wibowo <alif.likes.coding@gmail.com>
2022-10-26 16:55:38 +0700

    Make event listeners to the non action buttons

--- script.js
+++ script.js
@@ -20,3 +20,42 @@ let times = document.querySelector('#times');
 let divide = document.querySelector('#divide');
 let equal = document.querySelector('#equal');
 let mode = document.querySelector('#color-mode');
+
+/* User Variables */
+
+/* events */
+one.addEventListener('click', () => {
+    addToString(1);
+});
+two.addEventListener('click', () => {
+    addToString(2);
+});
+three.addEventListener('click', () => {
+    addToString(3);
+});
+four.addEventListener('click', () => {
+    addToString(4);
+});
+five.addEventListener('click', () => {
+    addToString(5);
+});
+six.addEventListener('click', () => {
+    addToString(6);
+});
+seven.addEventListener('click', () => {
+    addToString(7);
+});
+eight.addEventListener('click', () => {
+    addToString(8);
+});
+nine.addEventListener('click', () => {
+    addToString(9);
+});
+zero.addEventListener('click', () => {
+    addToString(0);
+});
+dot.addEventListener('click', () => {
+    addToString('.');
+});
+
+/* functions */
